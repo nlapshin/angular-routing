72c4002a49c47eb1fc6a635c1f5346ae0091dba2
Nikolay Lapshin <nlapshin1989@gmail.com>
2023-02-24 21:10:06 +0300

    Added lazy loading

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,20 +3,11 @@ import { HomeComponent } from './pages/home.component';
 import { AboutComponent } from './pages/about.component ';
 import { NotFoundComponent } from './pages/notFound.component';
 
-import { UsersMainComponent } from './pages/users/main.component';
-import { UsersListComponent } from './pages/users/list.component';
-import { UsersDetailComponent } from './pages/users/detail.component';
-
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   {
     path: 'users',
-    component: UsersMainComponent,
-    children: [
-      { path: '', redirectTo: 'list', pathMatch: 'full' },
-      { path: 'list', component: UsersListComponent },
-      { path: 'detail/:id', component: UsersDetailComponent },
-    ]
+    loadChildren: () => import('./features/users/users.module').then(m => m.UsersModule)
   },
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent, pathMatch:'full' },
---
+++ src/app/features/users/users.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { UsersMainComponent } from './pages/users.main.component';
+import { UsersListComponent } from './pages/users.list.component';
+import { UsersDetailComponent } from './pages/users.detail.component';
+
+@NgModule({
+  declarations: [
+    UsersMainComponent,
+    UsersListComponent,
+    UsersDetailComponent
+  ],
+  imports: [
+    RouterModule.forChild([
+      { path: '', redirectTo: 'list', pathMatch: 'full' },
+      { path: 'list', component: UsersListComponent },
+      { path: 'detail/:id', component: UsersDetailComponent },
+    ]),
+  ]
+})
+export class UsersModule { }
